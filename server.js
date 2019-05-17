3fdaf0a495e1307500ef964774310b5e01ee2853
joshua-unseen <corpselight@gmail.com>
2019-05-16 18:38:59 -0700

    Basic html pages

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const html = require("./app/routing/htmlRoutes");
+const api = require("./app/routing/apiRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
