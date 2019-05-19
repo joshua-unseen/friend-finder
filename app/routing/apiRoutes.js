704e1768d9a40d592f2e845f6cbf1d4ffbf3678a
joshua-unseen <corpselight@gmail.com>
2019-05-19 10:58:55 -0700

    Roughing in form submission

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,12 +3,21 @@ const path = require("path");
 const fs = require("fs");
 
 api.get("/friends", function(request, response) {
-    var friends = fs.readFileSync(path.join(__dirname, "../data/friends.js")).data;
+    // Using .readFileSync 'cuz I'd rather not deal with a callback inside a callback...
+    var friends = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/friends.js"), "utf-8"));
     return response.json(friends);
 });
 api.post("/friends", function(request, response) {
     var newFriend = request.body;
-    var friends = fs.readFileSync(path.join(__dirname, "../data/friends.js")).data;
+    var friends = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/friends.js"), "utf-8"));
+    // handle the comparison here, then
+    friends.push(newFriend);
+    console.log(newFriend);
+    response.json(newFriend);
+    // fs.writeFile(path.join(__dirname, "../data/friends.js"), JSON.stringify(friends), (err) => {
+    //     if (err) throw err;
+    //     console.log("friends.js updated.");
+    // });
 });
 
 module.exports = api;
